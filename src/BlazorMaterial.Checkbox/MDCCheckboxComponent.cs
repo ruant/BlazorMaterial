9a44ba49691aaac04494b6f75e2c3684019a1a8e
Rune Antonsen <rune@norbits.no>
2019-02-22 16:23:20 +0100

    feat(checkbox): start implementing checkbox

---
+++ src/BlazorMaterial.Checkbox/MDCCheckboxComponent.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Blazor.Components;
+using Microsoft.JSInterop;
+
+namespace BlazorMaterial
+{
+    public class MDCCheckboxComponent : BlazorMaterialComponent
+    {
+        private const string ADD_RIPPLE_FUNCTION = "mdc.ripple.MDCRipple.attachTo";
+        private static readonly ClassBuilder<MDCCheckboxComponent> _classNameBuilder;
+
+        [Parameter]
+        protected MDCButtonType Type { get; set; }
+        [Parameter]
+        protected MDCButtonStyle Style { get; set; }
+        [Parameter]
+        protected Action<UIMouseEventArgs> OnClick { get; set; }
+        [Parameter]
+        protected Action<UIMouseEventArgs> OnMouseUp { get; set; }
+        [Parameter]
+        protected Action<UIMouseEventArgs> OnMouseDown { get; set; }
+        [Parameter]
+        protected Action<UIKeyboardEventArgs> OnKeyPress { get; set; }
+        [Parameter]
+        protected Action<UIKeyboardEventArgs> OnKeyDown { get; set; }
+        [Parameter]
+        protected Action<UIKeyboardEventArgs> OnKeyUp { get; set; }
+        [Parameter]
+        protected string Icon { get; set; }
+        [Parameter]
+        protected bool Disabled { get; set; }
+        [Parameter]
+        protected bool Dense { get; set; }
+        [Parameter]
+        protected string HRef { get; set; }
+
+        [Parameter]
+        protected RenderFragment ChildContent { get; set; }
+
+        protected string ClassString { get; private set; }
+
+        protected ElementRef _MDCButton;
+        private bool _isFirstRender = true;
+
+        static MDCCheckboxComponent()
+        {
+            _classNameBuilder = new ClassBuilder<MDCCheckboxComponent>("mdc", "button")
+                .DefineClass("dense", c => c.Dense, PrefixSeparators.Modifier)
+                .DefineClass(c => c.Style.ToString().ToLowerInvariant(), c => c.Style != MDCButtonStyle.Default, PrefixSeparators.Modifier);
+        }
+
+        protected override void OnInit()
+        {
+            this.ClassString = _classNameBuilder.Build(this, this.Class);
+        }
+
+        protected override async Task OnAfterRenderAsync()
+        {
+            if (this._isFirstRender)
+            {
+                this._isFirstRender = false;
+                await JSRuntime.Current.InvokeAsync<bool>(ADD_RIPPLE_FUNCTION, this._MDCButton);
+            }
+        }
+    }
+}
